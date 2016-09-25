ae7737468d3a6fdfedc46bd9d7566b33bc3f58d5
Paulina Budzon <paulina.budzon@gmail.com>
2016-09-25 16:18:14 +0100

    Added snippet with ELB with Proxy policy

---
+++ src/elb_with_proxy_protocol.py
@@ -0,0 +1,35 @@
+from troposphere import Template
+from troposphere.elasticloadbalancing import LoadBalancer, Listener
+
+t = Template()
+
+t.add_description("ELB with Proxy Protocol enabled for ports 80 and 443")
+
+elb = t.add_resource(LoadBalancer(
+    "ElasticLoadBalancer",
+    Listeners=[
+        Listener(
+            LoadBalancerPort="80",
+            InstancePort="80",
+            Protocol="TCP"
+        ),
+        Listener(
+            LoadBalancerPort="443",
+            InstancePort="443",
+            Protocol="TCP"
+        ),
+    ],
+    Policies=[
+        {
+            "PolicyName": "EnableProxyProtocol",
+            "PolicyType": "ProxyProtocolPolicyType",
+            "Attributes": [{
+                "Name": "ProxyProtocol",
+                "Value": "true"
+            }],
+            "InstancePorts": ["80", "443"]
+        }
+    ]
+))
+
+print(t.to_json())
