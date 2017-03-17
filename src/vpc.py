71a0eb0ae3d58797aa7736889f36ae93e632cdc7
Paulina Budzon <paulina.budzon@gmail.com>
2017-03-17 13:47:06 +0000

    Updated VPC template to use GetAZs()

--- src/vpc.py
+++ src/vpc.py
@@ -1,14 +1,13 @@
-from troposphere import Join, Ref, Tags, Template
+from troposphere import GetAZs, Join, Ref, Select, Tags, Template
 from troposphere.ec2 import InternetGateway, NetworkAcl, NetworkAclEntry, Route, RouteTable, Subnet, \
     SubnetNetworkAclAssociation, SubnetRouteTableAssociation, VPC, VPCGatewayAttachment
 
-VPC_NETWORK = "172.21.0.0/16"
-VPC_PRIVATE_A = "172.21.1.0/24"
-VPC_PRIVATE_B = "172.21.2.0/24"
-VPC_PRIVATE_C = "172.21.3.0/24"
-VPC_PUBLIC_A = "172.21.128.0/24"
-VPC_PUBLIC_B = "172.21.129.0/24"
-VPC_PUBLIC_C = "172.21.130.0/24"
+VPC_NETWORK = "172.22.0.0/16"
+VPC_PRIVATE_1 = "172.22.1.0/24"
+VPC_PRIVATE_2 = "172.22.2.0/24"
+VPC_PUBLIC_A = "172.22.128.0/24"
+VPC_PUBLIC_1 = "172.22.129.0/24"
+VPC_PUBLIC_2 = "172.22.130.0/24"
 
 t = Template()
 
@@ -48,11 +47,19 @@ publicRouteTable = t.add_resource(RouteTable(
     ),
 ))
 
-privateRouteTable = t.add_resource(RouteTable(
-    "PrivateRouteTable",
+privateRouteTable1 = t.add_resource(RouteTable(
+    "PrivateRouteTable1",
     VpcId=Ref(vpc),
     Tags=Tags(
-        Name=Join("-", [Ref("AWS::StackName"), "private-rt"]),
+        Name=Join("-", [Ref("AWS::StackName"), "private-rt1"]),
+    ),
+))
+
+privateRouteTable2 = t.add_resource(RouteTable(
+    "PrivateRouteTable2",
+    VpcId=Ref(vpc),
+    Tags=Tags(
+        Name=Join("-", [Ref("AWS::StackName"), "private-rt2"]),
     ),
 ))
 
@@ -65,107 +72,73 @@ internetRoute = t.add_resource(Route(
 ))
 
 # private subnetworks
-subnetPrivateA = t.add_resource(Subnet(
-    "StackPrivateSubnetA",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "a"]),
-    CidrBlock=VPC_PRIVATE_A,
+subnetPrivate1 = t.add_resource(Subnet(
+    "StackPrivateSubnet1",
+    AvailabilityZone=Select(0, GetAZs()),
+    CidrBlock=VPC_PRIVATE_1,
     MapPublicIpOnLaunch=False,
     Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " private subnet A"]),
+        Name=Join("", [Ref("AWS::StackName"), " private subnet 1"]),
     ),
     VpcId=Ref(vpc)
 ))
 
 t.add_resource(SubnetRouteTableAssociation(
-    "PrivateSubnetARouteTable",
-    RouteTableId=Ref(privateRouteTable),
-    SubnetId=Ref(subnetPrivateA)
+    "PrivateSubnet1RouteTable",
+    RouteTableId=Ref(privateRouteTable1),
+    SubnetId=Ref(subnetPrivate1)
 ))
 
-subnetPrivateB = t.add_resource(Subnet(
-    "StackPrivateSubnetB",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "b"]),
-    CidrBlock=VPC_PRIVATE_B,
+subnetPrivate2 = t.add_resource(Subnet(
+    "StackPrivateSubnet2",
+    AvailabilityZone=Select(1, GetAZs()),
+    CidrBlock=VPC_PRIVATE_2,
     MapPublicIpOnLaunch=False,
     Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " private subnet B"]),
+        Name=Join("", [Ref("AWS::StackName"), " private subnet 2"]),
     ),
     VpcId=Ref(vpc)
 ))
 
 t.add_resource(SubnetRouteTableAssociation(
-    "PrivateSubnetBRouteTable",
-    RouteTableId=Ref(privateRouteTable),
-    SubnetId=Ref(subnetPrivateB)
-))
-
-subnetPrivateC = t.add_resource(Subnet(
-    "StackPrivateSubnetC",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "c"]),
-    CidrBlock=VPC_PRIVATE_C,
-    MapPublicIpOnLaunch=False,
-    Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " private subnet C"]),
-    ),
-    VpcId=Ref(vpc)
-))
-
-t.add_resource(SubnetRouteTableAssociation(
-    "PrivateSubnetCRouteTable",
-    RouteTableId=Ref(privateRouteTable),
-    SubnetId=Ref(subnetPrivateC)
+    "PrivateSubnet2RouteTable",
+    RouteTableId=Ref(privateRouteTable2),
+    SubnetId=Ref(subnetPrivate2)
 ))
 
 # public subnetworks
-subnetPublicA = t.add_resource(Subnet(
-    "StackPublicSubnetA",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "a"]),
-    CidrBlock=VPC_PUBLIC_A,
+subnetPublic1 = t.add_resource(Subnet(
+    "StackPublicSubnet1",
+    AvailabilityZone=Select(0, GetAZs()),
+    CidrBlock=VPC_PUBLIC_1,
     MapPublicIpOnLaunch=True,
     Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " public subnet A"]),
+        Name=Join("", [Ref("AWS::StackName"), " public subnet 1"]),
     ),
     VpcId=Ref(vpc),
 ))
 
 t.add_resource(SubnetRouteTableAssociation(
-    "PublicSubnetARouteTable",
+    "PublicSubnet1RouteTable",
     RouteTableId=Ref(publicRouteTable),
-    SubnetId=Ref(subnetPublicA)
+    SubnetId=Ref(subnetPublic1)
 ))
 
-subnetPublicB = t.add_resource(Subnet(
-    "StackPublicSubnetB",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "b"]),
-    CidrBlock=VPC_PUBLIC_B,
+subnetPublic2 = t.add_resource(Subnet(
+    "StackPublicSubnet2",
+    AvailabilityZone=Select(1, GetAZs()),
+    CidrBlock=VPC_PUBLIC_2,
     MapPublicIpOnLaunch=True,
     Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " public subnet B"]),
+        Name=Join("", [Ref("AWS::StackName"), " public subnet 2"]),
     ),
     VpcId=Ref(vpc),
 ))
 
 t.add_resource(SubnetRouteTableAssociation(
-    "PublicSubnetBRouteTable",
+    "PublicSubnet2RouteTable",
     RouteTableId=Ref(publicRouteTable),
-    SubnetId=Ref(subnetPublicB)
-))
-
-subnetPublicC = t.add_resource(Subnet(
-    "StackPublicSubnetC",
-    AvailabilityZone=Join("", [Ref("AWS::Region"), "c"]),
-    CidrBlock=VPC_PUBLIC_C,
-    MapPublicIpOnLaunch=True,
-    Tags=Tags(
-        Name=Join("", [Ref("AWS::StackName"), " public subnet C"]),
-    ),
-    VpcId=Ref(vpc)
-))
-
-t.add_resource(SubnetRouteTableAssociation(
-    "PublicSubnetCRouteTable",
-    RouteTableId=Ref(publicRouteTable),
-    SubnetId=Ref(subnetPublicC)
+    SubnetId=Ref(subnetPublic2)
 ))
 
 # network ACL for private subnets
@@ -178,20 +151,14 @@ privateNetworkAcl = t.add_resource(NetworkAcl(
 ))
 
 t.add_resource(SubnetNetworkAclAssociation(
-    "PrivateNetworkAAclAss",
-    SubnetId=Ref(subnetPrivateA),
+    "PrivateNetwork1AclAss",
+    SubnetId=Ref(subnetPrivate1),
     NetworkAclId=Ref(privateNetworkAcl)
 ))
 
 t.add_resource(SubnetNetworkAclAssociation(
-    "PrivateNetworkBAclAss",
-    SubnetId=Ref(subnetPrivateB),
-    NetworkAclId=Ref(privateNetworkAcl)
-))
-
-t.add_resource(SubnetNetworkAclAssociation(
-    "PrivateNetworkCAclAss",
-    SubnetId=Ref(subnetPrivateC),
+    "PrivateNetwork2AclAss",
+    SubnetId=Ref(subnetPrivate2),
     NetworkAclId=Ref(privateNetworkAcl)
 ))
 
